14fd89e77a4eb24f9c0de5dde481a143319f57fb
Steven Zhang <zhnewgm@gmail.com>
2015-01-12 22:59:04 -0500

    Added Sign Out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
